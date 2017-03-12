5485ec0097a51a78b134e718bea5ffc7d53831e0
YaroslavMatushevskyi <yaroslav.matushevskyy@gmail.com>
2017-03-12 15:17:18 +0200

    add new changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,108 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync').create();
-var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+'use strict';
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+var autoprefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    connect = require('gulp-connect'),
+    imagemin = require('gulp-imagemin'),
+    gulp = require('gulp'),
+    livereload = require('gulp-livereload'),
+    minifyCSS = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    pngquant = require('imagemin-pngquant'),
+    rename = require('gulp-rename'),
+    rigger = require('gulp-rigger'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglyfly = require('gulp-uglyfly');
 
-    browserSync.init({
-        server: "./app"
+
+// server connect
+gulp.task('connect', function() {
+    connect.server({
+        port: 5005,
+        root: './dist',
+        livereload: true
     });
-
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// html
+gulp.task('html', function() {
+    gulp.src('src/html/index.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload())
+        .pipe(notify({
+            message: 'I did (html)!',
+            onLast: true
+        }));
+});
+
+// sass
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(autoprefixer(['last 15 versions']))
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+    gulp.src('src/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minifyCSS())
+        .pipe(rename('app.min.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(connect.reload())
+        .pipe(notify({
+            message: 'I did (css)!',
+            onLast: true
+        }));
 });
 
-gulp.task('clearcache', function () {
-    return cache.clearAll();
+// js
+gulp.task('js', function() {
+    gulp.src('src/js/main.js')
+        .pipe(rigger())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglyfly())
+        .pipe(rename('app.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(connect.reload())
+        .pipe(notify({
+            message: 'I did (js)!',
+            onLast: true
+        }));
 });
 
-gulp.task('default', ['serve']);
+// img
+gulp.task('img', function() {
+    gulp.src('src/img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img/'))
+        .pipe(connect.reload())
+        .pipe(notify({
+            message: 'I did (img)!',
+            onLast: true
+        }));
+});
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch('src/html/**/*.html', ['html'])
+    gulp.watch('src/scss/**/*.scss', ['sass'])
+    gulp.watch('src/js/**/*.js', ['js'])
+    gulp.watch('src/img/**/*', ['img'])
+});
+
+// default
+gulp.task('default', ['connect', 'html', 'sass', 'js', 'watch']);
+
+// build
+gulp.task('build', ['html', 'sass', 'js', 'img']);
